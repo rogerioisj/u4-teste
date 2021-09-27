3aa8dd0f2f0eb6ea2624adb0fc102a91e0758443
rogerioisj <rogerio.reaper@gmail.com>
2021-09-27 18:23:57 -0300

    refactor: server file refactor

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
 import * as Hapi from '@hapi/hapi';
 import * as dotenv from 'dotenv';
 import {Server} from "@hapi/hapi";
+import {plugin} from "./plugins/routes";
 
 dotenv.config()
 
 const server: Server = Hapi.server({  port: process.env.PORT || 3000,  host: process.env.HOST || `localhost`,});
 
 export async function start(): Promise<Server> {
-  server.route({
+  /*server.route({
     method: 'GET',
     path: '/',
     handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
-      return h.response('OK')
+      return h.response('OK').code(200);
     }
-  })
+  })*/
+
+  await server.register(plugin);
 
   await server.start();
   console.log(`Server running on ${server.info.uri}`);
