ea3e3f47896574af2887dcd2551ff95cd41d9904
rogerioisj <rogerio.reaper@gmail.com>
2021-09-27 18:54:17 -0300

    refactor: unnecessary code removed

--- src/server.ts
+++ src/server.ts
@@ -8,14 +8,6 @@ dotenv.config()
 const server: Server = Hapi.server({  port: process.env.PORT || 3000,  host: process.env.HOST || `localhost`,});
 
 export async function start(): Promise<Server> {
-  /*server.route({
-    method: 'GET',
-    path: '/',
-    handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
-      return h.response('OK').code(200);
-    }
-  })*/
-
   await server.register(plugin);
 
   await server.start();
