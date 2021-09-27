0378a433508654304fb20b2b8232e84601ab9cc0
rogerioisj <rogerio.reaper@gmail.com>
2021-09-27 18:51:50 -0300

    feat: set enum for user

--- src/entities/register.entity.ts
+++ src/entities/register.entity.ts
@@ -1,5 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+export enum UserRole {
+    CLIENT = "client",
+    THIRD_PARTY = "third_party",
+}
+
 @Entity()
 export class Register {
     @PrimaryGeneratedColumn('uuid')
@@ -19,4 +24,11 @@ export class Register {
 
     @Column('varchar', { length: 10 })
     rg: string;
+
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.THIRD_PARTY
+    })
+    type: UserRole
 }
