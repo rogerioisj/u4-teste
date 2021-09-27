ea9aebad0778856243106488c3b39d3ea9dada64
rogerioisj <rogerio.reaper@gmail.com>
2021-09-27 19:15:55 -0300

    feat: car entity inserted

---
+++ src/entities/car.entity.ts
@@ -0,0 +1,10 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class Car {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('varchar', { length: 7})
+    placa: string;
+}
