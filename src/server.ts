e7c235e30a9f08b8736a9d34bdef5a0ad1014d6e
rogerioisj <rogerio.reaper@gmail.com>
2021-09-28 19:21:39 -0300

    feat: set auth strategy

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import * as dotenv from "dotenv";
 import { Server } from "@hapi/hapi";
 import { routes } from "./plugins/routes";
 import { createConnection } from "typeorm";
+const HapiJWT = require('hapi-jsonwebtoken');
+import { validate } from "./services/auth.service";
+import * as hapiAuthJwt2 from "hapi-auth-jwt2";
 
 dotenv.config();
 
@@ -21,6 +24,28 @@ const server: Server = Hapi.server({
 });
 
 export async function start(): Promise<Server> {
+  /*await server.register(HapiJWT.plugin);
+
+  server.auth.strategy("authenticate", "hapi-jsonwebtoken", {
+    secretOrPrivateKey: process.env.JWT_SECRET,
+    sign: {},
+    decode: {},
+    verify: {},
+    getToken: (request: Hapi.Request) => {
+      return request.headers.authorization;
+    },
+    validate,
+  });
+  server.auth.default("jwt");*/
+
+  await server.register(hapiAuthJwt2);
+  server.auth.strategy('jwt', 'jwt',
+      { key: process.env.JWT_SECRET,
+        validate
+      });
+
+  await server.auth.default('jwt');
+
   await server.register(routes);
 
   await server.start();
