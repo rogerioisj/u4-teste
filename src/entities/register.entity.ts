fbcb34a34fd7ab93b4b4cd346deea72caf968603
rogerioisj <rogerio.reaper@gmail.com>
2021-09-27 23:15:21 -0300

    refactor: refactor code with prettier

--- src/entities/register.entity.ts
+++ src/entities/register.entity.ts
@@ -1,34 +1,34 @@
-import {Column, Entity, PrimaryGeneratedColumn, Unique} from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 export enum UserRole {
-    CLIENT = "client",
-    THIRD_PARTY = "third_party",
+  CLIENT = "client",
+  THIRD_PARTY = "third_party",
 }
 
 @Entity()
 export class Register {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column('varchar', { length: 50})
-    name: string;
+  @Column("varchar", { length: 50 })
+  name: string;
 
-    @Column('varchar', { length: 11 })
-    cellphone: string;
+  @Column("varchar", { length: 11 })
+  cellphone: string;
 
-    @Column({type: 'varchar', length: 50, unique: true })
-    email: string;
+  @Column({ type: "varchar", length: 50, unique: true })
+  email: string;
 
-    @Column({type: 'varchar', length: 11, unique: true })
-    cpf: string;
+  @Column({ type: "varchar", length: 11, unique: true })
+  cpf: string;
 
-    @Column({type: 'varchar', length: 10, unique: true })
-    rg: string;
+  @Column({ type: "varchar", length: 10, unique: true })
+  rg: string;
 
-    @Column({
-        type: 'enum',
-        enum: UserRole,
-        default: UserRole.THIRD_PARTY
-    })
-    type: UserRole;
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.THIRD_PARTY,
+  })
+  type: UserRole;
 }
